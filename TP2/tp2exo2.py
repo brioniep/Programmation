c4a03fbccc5cccfb06e32e6c48086685b54532b3
brioniep <147743931+brioniep@users.noreply.github.com>
2023-10-26 11:04:36 +0200

    Add files via upload

---
+++ TP2/tp2exo2.py
@@ -0,0 +1,6 @@
+annee = 2023
+age = int(input("Donnez votre age : "))
+
+annee = annee - age
+
+print("Votre annee de naissance est : ", annee)
---
+++ TP2/tp2exo7.py
@@ -0,0 +1,8 @@
+import random
+
+x = random.randint(0,100)
+
+if x < 100*(2/3) :
+    print("c'est pile ! ")
+else :
+    print("c'est face ! ")
