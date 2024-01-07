efb2ea089e2d34a753d747870251bab739647d41
brioniep <147743931+brioniep@users.noreply.github.com>
2024-01-07 15:00:28 +0100

    Delete TP4 directory

--- TP4/tp4exo1.py
+++
@@ -1,8 +0,0 @@
-N = float(input("Vous cherchez la table de multiplication de quel nombre : "))
-T = [None] * 10
-
-
-for i in range(10):
-    
-    T[i] = N * i
-    print(N, "*" ,i ,"=" ,round(T[i], 10))
